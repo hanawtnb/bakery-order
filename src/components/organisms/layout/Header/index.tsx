8e8cb7d8855c98a87dd239cd5fed2394046a3fa3
hanawtnb <hanawtnb@icloud.com>
2022-03-02 09:49:14 +0900

    Headerの追加

--- src/components/organisms/layout/Header/index.tsx
+++ src/components/organisms/layout/Header/index.tsx
@@ -1,33 +1,35 @@
 import Link from "next/link";
 
+import styles from "./styles.module.scss";
+
 export const Header = () => {
   return (
     <>
-      <nav className="navBar">
-        <div className="container">
-          <Link href={"/"}>
-            <a className="title">Grain de ble</a>
+      <nav className={styles["navBar"]}>
+        <div className={styles["container"]}>
+          <Link href="/">
+            <a className={styles["title"]}>Grain de ble</a>
           </Link>
 
           <ul>
             <li>
-              <Link href={"/"}>
+              <Link href="/">
                 <a>注文</a>
               </Link>
             </li>
 
             <li>
-              <Link href={""}>
+              <Link href="">
                 <a>ログイン</a>
               </Link>
             </li>
             <li>
-              <Link href={""}>
+              <Link href="">
                 <a>新規登録</a>
               </Link>
             </li>
             <li>
-              <Link href={""}>
+              <Link href="">
                 <a>ログアウト</a>
               </Link>
             </li>
