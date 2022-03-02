c5e2011f62410972feaf46d26f8f5ae59a03c446
hanawtnb <hanawtnb@icloud.com>
2022-03-02 13:38:33 +0900

    Posterコンポーネントを追加

---
+++ src/components/molecules/Poster/index.tsx
@@ -0,0 +1,35 @@
+import React, { memo, VFC } from "react";
+import getConfig from "next/config";
+
+import styles from "./styles.module.scss";
+import Count from "../Count";
+import { useQuery } from "@apollo/client";
+import { ItemQUERY } from "api/query";
+import Link from "next/link";
+
+export const Poster = () => {
+  const { data, error, loading } = useQuery(ItemQUERY);
+  const { publicRuntimeConfig: config } = getConfig();
+
+  const onClickToDetail = () => {};
+  return (
+    <div className={styles["poster"]} onClick={onClickToDetail}>
+      {data?.items?.data.map(({ attributes, id }: any) => (
+        <Link key={id} href={{ pathname: "/item/[id]", query: { id } }}>
+          <a>
+            <div>
+              <h4>{attributes.name}</h4>
+              {attributes.image.data.map(({ attributes: att }: any) => (
+                <img src={`${config.api}${att?.url}`} key={att?.id} />
+              ))}
+              <p>{attributes.description}</p>
+              <p>{attributes.price}</p>
+            </div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default memo(Poster);
