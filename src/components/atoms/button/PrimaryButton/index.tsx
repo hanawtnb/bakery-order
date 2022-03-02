4f61fe436145e1d02380dfa1bebac8e36bb48007
hanawtnb <hanawtnb@icloud.com>
2022-03-02 13:39:17 +0900

    PrimaryButtonコンポーネントを追加

---
+++ src/components/atoms/button/PrimaryButton/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/display-name */
+import React, { memo } from "react";
+
+export const PrimaryButton = memo(() => {
+  return <button>カートに入れる</button>;
+});
+
+export default PrimaryButton;
