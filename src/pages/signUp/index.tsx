c9675c7fd11fd3455073c86c000096a9348a1e6b
hanawtnb <hanawtnb24@gmail.com>
2022-04-04 22:12:05 +0900

    React-hook-form, Yupを利用してバリデーションを実装

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -3,7 +3,11 @@ import { NextPage } from "next";
 import { useMutation } from "@apollo/client";
 import { RegisterMUTATION } from "api/query";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+import styles from "./styles.module.scss";
+import { PrimaryInput } from "../../components/atoms/input/index";
 interface IFormInput {
   userName: String;
   email: String;
@@ -15,17 +19,36 @@ const SignUp: NextPage = () => {
 
   const [signup, { loading }] = useMutation(RegisterMUTATION, {
     onCompleted: (res) => {
-      console.log(res);
-
       localStorage.setItem("token", res.register.jwt);
     },
   });
 
+  /**
+   * バリデーション.
+   */
+  let schema = yup.object().shape({
+    userName: yup.string().required("Please enter your user name"),
+    email: yup.string().email().required("Please enter your email"),
+    password: yup
+      .string()
+      .required("Please Enter your password")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+      ),
+    passwordConfirmation: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  });
+
+  /**
+   * フォームの送信.
+   */
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IFormInput>();
+  } = useForm<IFormInput | any>({ resolver: yupResolver(schema) });
   const onSubmit = (data: any) => {
     console.log(data);
     signup({
@@ -37,32 +60,43 @@ const SignUp: NextPage = () => {
         },
       },
     });
-    console.log(errors);
   };
 
   return (
     <div>
       {loading && <div>loading...</div>}
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="userName"
-          placeholder="userName"
-          {...register("userName", { required: true })}
-          // {...(errors.email && "Email is required")}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-          // {...(errors.email && "Email is required")}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          {...register("password", { required: true, min: 8 })}
-          // {...(errors.password && "Password is required")}
-        />
-        <input type="submit" />
+        <div className={styles["signUp"]}>
+          <PrimaryInput
+            type="text"
+            placeholder="User Name"
+            label="User Name"
+            register={register("userName")}
+            errorMessage={errors.userName?.message}
+          />
+          <PrimaryInput
+            type="email"
+            placeholder="Email"
+            label="Email"
+            register={register("email")}
+            errorMessage={errors.email?.message}
+          />
+          <PrimaryInput
+            type="password"
+            placeholder="Password"
+            label="Password"
+            register={register("password")}
+            errorMessage={errors.password?.message}
+          />
+          <PrimaryInput
+            type="password"
+            placeholder="Confirmation Password"
+            label="Confirmation Password"
+            register={register("confirmationPassword")}
+            errorMessage={errors.confirmationPassword?.message}
+          />
+          <input type="submit" />
+        </div>
       </form>
     </div>
   );
