be3fddb0f38c7bad7124dd99e7d5c2663251d3c0
hanawtnb <hanawtnb24@gmail.com>
2022-04-06 13:24:15 +0900

    カートに追加機能を実装

--- src/components/molecules/MenuPoster/index.tsx
+++ src/components/molecules/MenuPoster/index.tsx
@@ -1,40 +1,91 @@
-import { useQuery } from "@apollo/client";
-import { MenuQUERY } from "api/query";
-import React from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
+
+import { useMutation } from "@apollo/client";
 import getConfig from "next/config";
+import { useRecoilState } from "recoil";
 
 import styles from "./styles.module.scss";
 import PrimaryButton from "src/components/atoms/button/PrimaryButton";
 import Quantity from "../Quantity";
+import { UpdateCartMUTATION } from "src/api/query";
+import { useCartsQuery, useMenusQuery } from "src/types/generated/graphql";
+import { cartState } from "../../../store/cartAtom";
 
-const MenuPoster = () => {
-  const { data, error, loading } = useQuery(MenuQUERY);
-  const { publicRuntimeConfig: config } = getConfig();
-  console.log("これ", data);
-
-  return (
-    <>
-      <div className={styles["poster"]}>
-        {data?.menu?.data.attributes?.items.data.map(
-          ({ attributes, id }: any) => (
-            <>
-              <div key={id}>
-                <h4>{attributes.name}</h4>
-                {attributes.image.data.map(({ attributes: att, id }: any) => (
-                  <img src={`${config.api}${att?.url}`} key={id} />
-                ))}
-                <p>{attributes.description}</p>
-                <p>{attributes.price}</p>
-              </div>
-              <Quantity />
-              <PrimaryButton>カートに入れる</PrimaryButton>
-              <PrimaryButton>削除</PrimaryButton>
-            </>
-          )
-        )}
-      </div>
-    </>
-  );
+type Props = {
+  id: number;
+  attributes: any;
 };
 
+const MenuPoster: FC<Props> = ({ id, attributes }) => {
+  const { publicRuntimeConfig: config } = getConfig();
+  const [qty, setQty] = useState(0);
+  const [userId, setUserId] = useState("");
+  const [cartList, setCartList]: Array<any> = useRecoilState(cartState);
+  const [addCart, { error }] = useMutation(UpdateCartMUTATION, {
+    onCompleted: (res) => {
+      error && console.log(error);
+    },
+  });
+
+  useEffect(() => {
+    setUserId(localStorage.getItem("userId") as string);
+  }, []);
+
+  // const { data: cartData } = useCartsQuery({
+  //   variables: {
+  //     filters: {
+  //       users_permissions_user: {
+  //         id: {
+  //           eq: userId,
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+
+  const onClickAddCart = (attribute: any) => {
+    console.log("ID", id);
+    setCartList(
+      cartList
+        ? [...cartList, { ...attribute, quantity: qty }]
+        : [{ ...attribute, quantity: qty }]
+    );
+    // addCart({
+    //   variables: {
+    //     updateCartId: cartData?.carts?.data[0].id,
+    //     data: {
+    //       item: {
+    //         data: {
+    //           attributes: [
+    //             {
+    //               id,
+    //               qty,
+    //             },
+    //           ],
+    //         },
+    //       },
+    //     },
+    //   },
+    // });
+  };
+  console.log("カートの中身", cartList);
+
+  return (
+    <div className={styles["poster"]} key={id}>
+      <div>
+        <h4>{attributes.name}</h4>
+        {attributes.image.data.map(({ attributes: att, id }: any) => (
+          <img src={`${config.api}${att?.url}`} key={id} />
+        ))}
+        <p>{attributes.description}</p>
+        <p>{attributes.price}</p>
+      </div>
+      <Quantity qtyFunc={(qty) => setQty(qty)} />
+      <PrimaryButton onClick={() => onClickAddCart(attributes)}>
+        カートに入れる
+      </PrimaryButton>
+      <PrimaryButton>削除</PrimaryButton>
+    </div>
+  );
+};
 export default MenuPoster;
