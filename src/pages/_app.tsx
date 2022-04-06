db4b5ce728638a3fab28670bcf43a91c01fdbcec
hanawtnb <hanawtnb24@gmail.com>
2022-04-06 12:06:10 +0900

    Apollo clientのcacheをlocalstorageに保存する設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,40 @@
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { LocalStorageWrapper, persistCache } from "apollo3-cache-persist";
 
 import client from "../../graphql/apollo";
-
 import "../../styles/globals.scss";
+import getConfig from "next/config";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const { publicRuntimeConfig: config } = getConfig();
+
+const httpLink = createHttpLink({
+  uri: `${config.api}/graphql`,
+});
+const cache = new InMemoryCache();
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  if (typeof window !== "undefined") {
+    persistCache({
+      cache,
+      storage: new LocalStorageWrapper(window.localStorage),
+    });
+  }
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache,
+  });
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
   );
-}
+};
 
 export default MyApp;
